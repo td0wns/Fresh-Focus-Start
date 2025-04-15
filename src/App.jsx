81d080a859fdd4ade2536dfb909288fba528bebe
td0wns <trevor.p.downs@gmail.com>
2025-04-15 13:25:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -402,7 +402,7 @@ if (valid) {
         </>
       )}
 
-      {gamePhase === "enterWords" && timer === 0 && (
+     {gamePhase === "gameOver" && (
         <div style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "rgba(0,0,0,0.6)", display: "flex", alignItems: "center", justifyContent: "center", zIndex: 9999 }}>
           <div style={{ backgroundColor: "white", padding: "2rem", borderRadius: "1rem", maxWidth: "500px", textAlign: "left" }}>
             <h2 style={{ textAlign: "center", color: "#786daa", marginBottom: "1rem" }}>Well done!</h2>
